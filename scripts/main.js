9ef7aeeea7c9ecb4d4117b11b56d799c36c98fdb
Kevin Vilyan <146146416+Kev-Vily@users.noreply.github.com>
2025-07-20 20:25:07 +0700

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const mindusindo = Vars.mods.locateMod("terjemahan-id-tempo-doeloe").meta;
+
+mdidtempodoeloe.displayName = Core.bundle.get("mod.terjemahan-id-tempo-doeloe.name");
+mdidtempodoeloe.author = Core.bundle.get("mod.terjemahan-id-tempo-doeloe.author");
+mdidtempodoeloe.description = Core.bundle.get("mod.terjemahan-id-tempo-doeloe.description");
+mdidtempodoeloe.subtitle = Core.bundle.get("mod.terjemahan-id-tempo-doeloe.subtitle");
